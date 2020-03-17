8662b435afb7e49ea8a54e6854cc0d1d58971e1a
Justas Mundeikis <mundeikis@gmx.de>
2020-03-17 15:55:47 +0200

    update

---
+++ scripts/figure_1.R
@@ -0,0 +1,23 @@
+data_world <- read.csv("./data/data_world.csv",
+                       stringsAsFactors = FALSE)%>%
+        mutate(date=as.Date(date))%>%
+        mutate(var=factor(var, levels=c("active", "recovered", "deaths")))%>%
+        filter(var!="confirmed")%>%
+        group_by(date, var)%>%
+        summarise(value=sum(value))
+
+max.date <- max(data_world$date)
+
+
+ggplot(data_world, aes(x=date, y=value, fill=var))+
+        geom_area(alpha=0.5)+
+        scale_fill_manual(values=c("red", "green", "black"))+ 
+        scale_x_date(breaks="1 week")+
+        scale_y_continuous(breaks =pretty_breaks())+
+        labs(title=paste0("COVID-19 (last update ", max.date,")"),
+             subtitle = "Source: 2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE",
+             x="Date",
+             y="Count")+
+        theme(legend.title=element_blank(),
+              legend.position='bottom',
+              axis.text.x = element_text(angle = 45, hjust = 1))
