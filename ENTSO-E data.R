b1329a7368f6a066938546af8d08877f1460d7ca
Rokis1990 <rokas.klydzia@gmail.com>
2020-03-23 00:19:34 +0200

    simple weekly plots

--- ENTSO-E data.R
+++ ENTSO-E data.R
@@ -27,3 +27,31 @@ plot(total_load_2019$Actual.Total.Load..MW....BZN.LT)
 plot(total_load_2019$Day.ahead.Total.Load.Forecast..MW....BZN.LT)
 plot.ts(total_load_2019$Actual.Total.Load..MW....BZN.LT)
 plot.ts(total_load_2019$Day.ahead.Total.Load.Forecast..MW....BZN.LT)
+
+ts.plot(total_load_2020$Actual.Total.Load..MW....BZN.LT[24])
+
+library(forecast)
+
+load <- msts(total_load_2020$Actual.Total.Load..MW....BZN.LT, start = c(2020,1), seasonal.periods = c(8760, 52))
+fit <- tbats(load)
+plot(forecast(fit))
+plot(load)
+
+?tbats
+?ts
+?msts
+
+
+ts.plot(load[1:168])
+ts.plot(load[168:336])
+ts.plot(load[336:504])
+ts.plot(load[504:672])
+ts.plot(load[672:840])
+ts.plot(load[840:1008])
+ts.plot(load[1008:1176])
+ts.plot(load[1176:1344])
+ts.plot(load[1344:1512])
+ts.plot(load[1512:1680])
+ts.plot(load[1680:1848])
+ts.plot(load[1848:2016])
+
